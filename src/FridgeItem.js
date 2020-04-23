92da5be56c57ede3f98e63a65a940576792ff7f2
ted <teddsbennett@gmail.com>
2020-04-23 07:09:35 +0100

    Display Name, Quantity and expiration data

--- src/FridgeItem.js
+++ src/FridgeItem.js
@@ -8,9 +8,32 @@ const Container = styled.div`
     border-radius: 8px;
 `;
 
+const QuantityBox = styled.div`
+    width: 5%;
+    height: 100%;
+    display: inline-block;
+`;
+
+const TitleBox = styled.div`
+    width:85%;
+    display: inline-block;
+`;
+
+const ExpiryBox = styled.div`
+    display: inline-block;
+    width:10%;
+    text-align:right;
+`;
+
 export class FridgeItem extends Component {
     render() {
-        return <Container>{this.props.item.title}</Container>;
+        return <Container>
+            <QuantityBox>{this.props.item.quantity}x</QuantityBox>
+            
+            <TitleBox>{this.props.item.title}</TitleBox>
+            
+            <ExpiryBox>{this.props.item.expiry}</ExpiryBox>
+            </Container>;
     }
 }
 
